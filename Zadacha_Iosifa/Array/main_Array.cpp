e2d94e33b2c27fdf545518b72a85e89789a11ac1
unknown <denisasanov67@gmail.com>
2026-03-23 12:00:23 +0800

    Josephus's Problem

--- Zadacha_Iosifa/Array/main_Array.cpp
+++ Zadacha_Iosifa/Array/main_Array.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "array.h"
-
+#include "ctime"
 using namespace std;
 
+/*
 int main(void)
 {
     setlocale(LC_ALL, "ru");
@@ -14,39 +15,74 @@ int main(void)
         }
         cout << "Начальный массив:" << endl << arr << endl;
 
-        
+
 
         for (int i = 0; i < 8; i += 2) {
             arr.insert(10 + i, i);
         }
         cout << "После вставки четных чисел:" << endl << arr << endl;
 
-       
+
         for (int i = 1; i < 8; i += 2) {
             arr[i] = 20 + i;
         }
         cout << "После изменения через []:" << endl << arr << endl;
 
-     
+
         for (int i = 6; i >= 0; i -= 3) {
             arr.remove(i);
         }
         cout << "После удаления:" << endl << arr << endl;
 
-  
-        Array arrCopy = arr; 
+
+        Array arrCopy = arr;
         Array arr2(2);
         arr2 = arr;
-
         cout << "Копия массива:" << endl << arrCopy << endl;
-
-      
-        cout << arr[100] << endl; 
+        cout << arr[100] << endl;
 
     }
     catch (ArrayException& e) {
         cerr << "Произошла ошибка: выход за границы массива!" << endl;
     }
 
+    return 0;
+}
+*/
+int JosephProblem(int N, int k) {
+
+    Array arr(N);
+    for (int i = 0; i < N; i++) {
+        arr.insert(i);
+    }
+    int index = 0;
+    int currentSize = N;
+
+    while (arr.getSize() > 1) {
+        index = (index + k - 1) % arr.getSize();
+
+        arr.remove(index);
+        currentSize--;
+
+        if (index == currentSize) index = 0;
+    }
+    return arr[0];
+}
+
+int main(void)
+{
+    setlocale(LC_ALL, "ru");
+    int test[] = { 1000,5000,10000,50000,100000 };
+    int size = sizeof(test) / sizeof(test[0]);
+
+    for (int i = 0; i < size; i++) {
+        int N = test[i];
+        int k = 2;
+        clock_t start = clock();
+        int res = JosephProblem(N, k);
+        clock_t end = clock();
+        double time = double(end - start) / CLOCKS_PER_SEC;
+        cout << N << "\t\t" << res << "\t\t"<< time <<" сек." << endl;
+    }
     return 0;
 }
