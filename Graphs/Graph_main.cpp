4a1e327fe247ec3ecd9c5018fb891e1a43f7a961
denisasanov67-design <denisasanov67@gmail.com>
2026-06-01 09:55:24 +0300

    Update Graph_main.cpp

--- Graphs/Graph_main.cpp
+++ Graphs/Graph_main.cpp
@@ -6,36 +6,31 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Russian");
 
-     
-    cout « "TestGraph.txt" « endl;
-    Graph graph("Тест 1: TestGraph.txt");
-    
-    // очистка файлов перед запуском
-    graph.cleanupFiles("TestGraph_component_*.txt");
-    graph.cleanupFiles("1000_component_*.txt");
-    vector<vector<Node*>> components = graph.connectedComponents();
+    // Тест 1
+    cout << "Тест 1: TestGraph.txt" << endl;
+    Graph graph("TestGraph.txt");
 
-    graph.saveComponents(components, "TestGraph_");
+    // очистка старых файлов перед запуском 
+    graph.cleanupOldFiles("TestGraph_component_*.txt");
+    graph.cleanupOldFiles("1000_component_*.txt");
+
+    vector<vector<Node*>> components1 = graph.connectedComponents();
+    graph.saveComponents(components1, "TestGraph_");
 
     BFS bfs(graph);
     Node* n1 = graph.findNode("0");
     Node* n2 = graph.findNode("20");
-
     if (n1 && n2) {
-        cout « "BFS: вершины 0 и 20 "
-            « (bfs.connection(n1, n2) ? "связаны" : "не связаны") « endl;
+        cout << "BFS: вершины 0 и 20 "
+            << (bfs.connection(n1, n2) ? "связаны" : "не связаны") << endl;
     }
 
-    cout <<"Тест 2: 1000.csv" << endl;
-    ifstream check("1000.csv");
-        
+    //  ТЕСТ 2 
+    cout << "\nТест 2: 1000.csv " << endl;
+
     Graph graph2("1000.csv");
     vector<vector<Node*>> components2 = graph2.connectedComponents();
-
     graph2.saveComponents(components2, "1000_");
-    cout « "\nВсе файлы сохранены в текущую директорию: " « endl;
-    system("ls -la component_*.txt 2>/dev/null || dir component_*.txt");
-    
-            
+
     return 0;
 }
